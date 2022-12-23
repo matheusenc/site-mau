708c570eb3fe4c851211fbaca91be0f60b512bd0
Matheus Estevam <68551343+matheusenc@users.noreply.github.com>
2022-12-22 23:34:36 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+//nav-bar
+class MobileNavbar {
+    constructor(mobileNav, navList, navLinks) {
+        this.mobileNav = document.querySelector(mobileNav);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation ?
+                (link.style.animation = "") :
+                (link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+0.3}s`);
+        })
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileNav.classList.toggle(this.activeClass);
+        this.animateLinks()
+    }
+
+    addClickEvent() {
+        this.mobileNav.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileNav) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-nav",
+    ".nav-list",
+    ".nav-list li",
+);
+
+mobileNavbar.init();
+
